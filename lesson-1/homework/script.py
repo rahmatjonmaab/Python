72645cc49d0090153281f62590622b718be55c69
rahmatjonmaab <urazbaevrahmajon@gmail.com>
2025-08-20 19:55:45 +0500

    Create script.py

---
+++ lesson-1/homework/script.py
@@ -0,0 +1,33 @@
+##Kvadratning yuzi
+a=5
+S=a**2
+print(S)
+
+##Doiraning uzuligi
+d=10
+A=d*3.14
+print(A)
+
+##Ikkita son a va b berilgan. Ularning o‘rtacha qiymatini toping.
+a=10
+b=20
+c=(a+b)/2
+print(c)
+
+##Ikkita son a va b berilgan. Ularning yig‘indisi, ko‘paytmasi va har birining kvadratini toping.
+a=10
+b=20
+c=a+b
+print(c)
+
+##Ikkita son a va b berilgan. Ularning ko‘paytmasi toping.
+a=10
+b=20
+c=a*b
+print(c)
+
+##Ikkita son a va b berilgan. Ularning har birining kvadratini toping.
+a=10**2
+b=20**2
+
+print(a,b)
